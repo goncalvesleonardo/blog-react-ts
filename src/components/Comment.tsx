a61200d537605db2f847fea76425e0161f774348
Leonardo Oliveira Gonçalves <oleonardo.goncalves@gmail.com>
2023-08-04 16:10:40 -0300

    inittial commit

---
+++ src/components/Comment.tsx
@@ -0,0 +1,56 @@
+import React, { useState } from 'react';
+
+import styles from './Comment.module.css'
+import { ThumbsUp, Trash } from '@phosphor-icons/react';
+import { Avatar } from './Avatar';
+
+interface CommentProps {
+  content: string,
+  onDeleteComment: (comment: string) => void
+}
+
+export function Comment({content, onDeleteComment}: CommentProps) {
+  const [likeCount, setLikeCount] = useState(0);
+
+  function handleDeleteComment() {
+    onDeleteComment(content);
+  }
+
+  function handleLikeComment() {
+    setLikeCount((state) => {
+      return state + 1
+    })
+  }
+
+  return (
+    <div className={styles.comment}>
+      <Avatar hasBorder={false} src="https://github.com/goncalvesleonardo.png" alt="" />
+      
+      <div className={styles.commentBox}>
+        
+        <div className={styles.commentContent}>
+          <header>
+            <div className={styles.authorAndTime}>
+              <strong>Leonardo Gonçalves</strong>
+              <time title="11 de Maio as 08:00" dateTime='2022-05-11 08:00:00'>Cerca de 1h atrás</time>
+            </div>
+
+            <button onClick={handleDeleteComment} title='Deletar comentário'>
+              <Trash size={24}/>
+            </button>
+          </header>
+
+          <p>{content}</p>  
+        </div>
+        <footer>
+          <button onClick={handleLikeComment}>
+            <ThumbsUp />
+            Aplaudir <span>{likeCount}</span> 
+          </button>
+        </footer>
+
+      </div>
+
+    </div>
+  )
+}
